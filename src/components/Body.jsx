2f2b7ab11fd79f707d7c59d33f67dd75b314e1c8
Ashish Pandey <ap1700080@gmail.com>
2026-03-21 22:46:21 +0530

    now used the authstate changed hook that will be run once and send the details for the user signin or signout and dispatch the action of adduser and remove user on the signin and signout of the user from the redux store

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './Login'
 import Browse from './Browse'
+import {onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+
+
+
+
   const path = createBrowserRouter([
     {
         path:"/",
@@ -13,6 +21,19 @@ import Browse from './Browse'
     }
   ])
 const Body = () => { 
+   const dispatch = useDispatch();
+  
+
+ useEffect(()=>{
+       onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const {uid , email ,displayname }= user;
+        dispatch(addUser({uid:uid,email:email, displayname:displayname}))
+    } else {
+        
+    }
+});
+ },[])
 
   return (
    <RouterProvider router= {path}/>
